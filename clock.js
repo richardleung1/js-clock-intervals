3fe76ed8d607f0145cc47b9a2f39cc83df7d5da3
Richard Leung <michelleli@MMac.local>
2020-10-23 17:45:46 -0400

    [clock.js] Complete clock

--- clock.js
+++ clock.js
@@ -1,24 +1,35 @@
-function secondRotation(seconds) {
-    let degRotation = seconds * 6;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
     
-      return degRotation;
-    }
+function secondRotation(seconds) {
+    let degRotation = (seconds % 60) * 6;
+    return degRotation;
+}
 
 function minuteRotation(minutes, seconds) {
-    minutes = minutes += (seconds/60);
-        let degRotation = minutes *.1;
-        return degRotation;
+    let degRotation = minutes * 6;
+    return degRotation;
 }
-        
-
 
 function hourRotation(hours, minutes) {
-    minute = minutes += (hours*60);
-    let degRotation = minutes *.5;
+    let degRotation = (hours * 30);
     return degRotation;
 }
 
 function tick() {
-    setInterval(() => console.log("a"), 1000)
+    seconds = seconds += 1;
+    minutes = seconds / 60
+    hours = seconds / 3600
+    
+    let hourHand = document.getElementById("hour")
+    let hoursDegrees = hourRotation(hours, minutes)
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+    let minuteHand = document.getElementById("minute")
+    let minutesDegrees = minuteRotation(minutes, seconds);
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
+    let secondHand = document.getElementById("second")
+    let secondsDegrees = secondRotation(seconds);
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
 }
-tick()
+setInterval(tick, 1000)
