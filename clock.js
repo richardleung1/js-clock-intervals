78c9272757ec8f1324f6cd8fd158b2b4ebffcb12
Ariel <Ariel@Maddies-MacBook-Pro.local>
2020-10-23 14:17:03 -0400

    [Java] added second minute andhour functions

--- clock.js
+++ clock.js
@@ -3,3 +3,18 @@ function secondRotation(seconds) {
     
       return degRotation;
     }
+
+function minuteRotation(minutes, seconds) {
+    minutes = minutes += (seconds/60);
+        let degRotation = minutes *.1;
+        return degRotation;
+}
+        
+
+
+function hourRotation(hours, minutes) {
+    minute = minutes += (hours*60);
+    let degRotation = minutes *.5;
+    return degRotation;
+}
+
